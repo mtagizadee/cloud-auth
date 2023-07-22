15cb8997c289c93a1995bad4a13d58e9f6417d78
mtagizadee <magatagi0@gmail.com>
2023-07-23 00:38:49 +0400

    gin was added

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+
+
+	
 
 func main() {
-	fmt.Println("Hello, world");
+	r := gin.Default()
+	
+	api := r.Group("/api")
+	v1 := api.Group("/v1")
+
+	v1.GET("/ping", ping)
+	r.Run("localhost:3001") 
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
