945dca1881c749ac347f3fdac2924b6e7c44446c
mtagizadee <magatagi0@gmail.com>
2023-07-23 17:33:39 +0400

    requests now can be accepted only from the middleware

--- main.go
+++ main.go
@@ -40,6 +40,20 @@ func main() {
 	}
 	fmt.Println("Database Migrated")
 
+	// requests can be accepted only from localhost:8080
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		
+		c.Next()
+	})
+		
 	auth := r.Group("/auth")
 	auth.POST("/signup", signup)
 	auth.POST("/login", login)
